1e51b757b526b471a012f34ba357dff9c20373fb
Bede Kelly <bedekelly97@gmail.com>
2017-07-19 16:31:49 +0100

    Add comments and reformat template routes file.

--- template/routes.c
+++ template/routes.c
@@ -1,16 +1,20 @@
 #include "microservice.h"
 
 
+// Return the original request, wrapped in our object.
 json_response ping(json request) {
-  return ok(request);
+  return ok(kv("Original Request", request));
 }
 
+
+// Return a friendly "Hello world" response.
 json_response hello() {
   return msg("Hello, world!");
 }
 
-void routes(method get, method post)
-{
+
+// Route both methods to the "/" endpoint.
+void routes(method get, method post) {
   route(post, "/", ping);
   route(get, "/", hello);
 }
